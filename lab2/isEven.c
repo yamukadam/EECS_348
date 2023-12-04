1e5d54a6ab1216b3b976c1384eba3f6070f05e78
Yaeesh <mukadam.yaeesh@gmail.com>
2023-12-04 00:11:12 -0600

    Remove all files and directories

--- lab2/isEven.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "isEven.h"
-int isEven(int num)
-{
-	if (num % 2 == 0){
-		 printf("%d is even.\n", num);
-	}
-	return 0;
-}
--- lab2/isOdd.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "isOdd.h"
-int isOdd(int num)
-{
-	if (num % 2 == 1){
-		 printf("%d is odd.\n", num);
-	}
-	return 0;
-}
